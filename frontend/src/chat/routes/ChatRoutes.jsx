a71f3fd1ab48107d401437458d6a4d298a8f0913
Andrea Mendoza <106608657+Andrea-MT@users.noreply.github.com>
2025-11-15 19:58:41 -0600

    feat: 🚀 Create admin page

--- frontend/src/chat/routes/ChatRoutes.jsx
+++ frontend/src/chat/routes/ChatRoutes.jsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { ProfilePage } from '../pages/ProfilePage';
+import { AdminPage } from '../pages/admin/AdminPage';
 import { ChatLayout } from '../components/ChatLayout';
 import '../chat.css';
 
@@ -8,6 +9,7 @@ export const ChatRoutes = () => {
         <Routes>
             <Route element={<ChatLayout />}>
                 <Route path="profile" element={<ProfilePage />} />
+                <Route path="admin" element={<AdminPage />} /> 
                 <Route path="/*" element={<Navigate to={'/'} />} />
             </Route>
         </Routes>
